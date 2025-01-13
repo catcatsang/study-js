8b6787fa7cf4deded66215f81dc88e8416430e4c
catcatsang <rksel0712@gmail.com>
2025-01-13 18:50:04 +0900

    bukking_clone

---
+++ clone_coding/banner.js
@@ -0,0 +1,17 @@
+const Banner = document.getElementByClassName("banner");
+const arrow = document.getElementsByClassName("");
+
+let count = 1;
+let tempbutton = buttons[0];
+firstBanner.innerHTML = `<img
+                  src="https://img.echosting.cafe24.com/ec/shop_kr/v2/youtubeshopping/affiliate/img_affili_main.png"
+                  alt="affiliate"
+                />`;
+
+lastBanner.innerHTML = `<img
+                  src="https://img.echosting.cafe24.com/ec/shop_kr/v2/main/img_visual_pro.png"
+                  alt=""
+                />`;
+Banner.appendChild(firstBanner);
+Banner.prepend(lastBanner);
+Banner.style.transform = `translate(-500px)`;
